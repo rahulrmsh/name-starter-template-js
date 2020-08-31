677ad27b07dbc659bd955182b9bf59bdc8af075b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-31 15:26:35 +0530

    js added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
+var speedCount = 0;
 document.querySelector('#submit').addEventListener('click', function() {
     var inputValue = document.querySelector('#listText').value;
     if (inputValue !== null && inputValue !== null && inputValue !== undefined && inputValue !== 0 && inputValue !== "" && inputValue !== false && inputValue !== NaN) {
-        var innerDiv = document.createElement('div');
-        innerDiv.className = 'container-md themed-container text-muted';
-        innerDiv.id = listKey;
-        innerDiv.style = colorList[numKey];
-        innerDiv.innerHTML = (listKey + 1) + '.\t' + inputValue;
-        document.querySelector('#listKey').appendChild(innerDiv);
-        document.querySelector('#listText').value = '';
-        if (numKey < 5) {
-            numKey++;
-        } else {
-            numKey = 0;
+        document.querySelector('.wrapper').innerHTML = '';
+        for (var i = 0; i < inputValue.length; i++) {
+            speedCount += 120;
+            var animationSpeed = 'animation-delay: ' + speedCount + 'ms;';
+            var innerSpan = document.createElement('span');
+            innerSpan.style = animationSpeed;
+            innerSpan.innerHTML = inputValue.charAt(i);
+            document.querySelector('.wrapper').appendChild(innerSpan);
+            document.querySelector('#listText').value = '';
         }
-        listKey++;
     }
-})
-
-document.querySelector('#reset').addEventListener('click', function() {
-    document.querySelector('#listKey').innerHTML = '';
-    numKey = 0;
 })
