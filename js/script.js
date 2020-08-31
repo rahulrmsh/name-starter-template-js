f669eca253bda4f6a3922b109dad6ffb9c311b39
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-31 15:31:51 +0530

    js added

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ document.querySelector('#submit').addEventListener('click', function() {
     if (inputValue !== null && inputValue !== null && inputValue !== undefined && inputValue !== 0 && inputValue !== "" && inputValue !== false && inputValue !== NaN) {
         document.querySelector('.wrapper').innerHTML = '';
         for (var i = 0; i < inputValue.length; i++) {
-            speedCount += 120;
-            var animationSpeed = 'animation-delay: ' + speedCount + 'ms;';
             var innerSpan = document.createElement('span');
-            innerSpan.style = animationSpeed;
-            innerSpan.innerHTML = inputValue.charAt(i);
+            if (inputValue.charAt(i) !== " ") {
+                speedCount += 120;
+                var animationSpeed = 'animation-delay: ' + speedCount + 'ms;';
+                innerSpan.style = animationSpeed;
+                innerSpan.innerHTML = inputValue.charAt(i);
+            } else {
+                innerSpan.innerHTML = '&nbsp;';
+            }
             document.querySelector('.wrapper').appendChild(innerSpan);
             document.querySelector('#listText').value = '';
         }
